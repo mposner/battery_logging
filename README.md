97615a58ae3bf9840e87aa8b6a8f3625e1ff9acb
mposner <mike24hockey@gmail.com>
2014-07-27 18:07:13 -0700

    added argument parsing and file logging

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Windows Battery Logging
 
 This script is designed to be run periodically and will gather 
 current battery data and append it to a file.
+
+Information on the WMIC Win32_Battery class and the units of its properties can
+be found at:  http://msdn.microsoft.com/en-us/library/aa394074(v=vs.85).aspx
+-----------------------
+
+Inspired by http://www.ifweassume.com/2013/08/the-de-evolution-of-my-laptop-battery.html
--- battery.py
+++ battery.py
@@ -4,17 +4,48 @@ Windows Battery Status Logging
 7/27/14
 """
 
+import sys
 import subprocess
+from datetime import datetime
 
+# Check and parse input arguments
+if len(sys.argv) > 2:
+	usage()
+	exit(1)
+elif len(sys.argv) == 2:
+	logfile = sys.argv[1]
+elif len(sys.argv) == 1:
+	logfile = ""
+
+# WMIC Win32_Battery fields to query
 battery_vars = ['BatteryStatus', 'EstimatedChargeRemaining', 'EstimatedRunTime',
 				'Status', 'TimeOnBattery','TimeToFullCharge']
 
-result = ""
-				
-for var in battery_vars:			
-	temp = subprocess.check_output(["wmic", "path", "Win32_Battery", "get", var, 
-									   "/value"])	
-	result += ' ' + temp.strip()
+# Start data with timestamp
+result = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+for var in battery_vars:
+
+	#execute wmic command and capture output
+	temp = subprocess.check_output(["wmic", "path", "Win32_Battery", "get", var, "/value"])	
 	
-print "====== Result: "
-print result
+	result += ', ' + temp.strip()
+	
+# Log data to given file, or stdout if no logfile was provided
+result = '[' + result + ']\n'
+
+if logfile == "":
+	print result
+else:
+	with open(logfile, 'a') as f:
+		f.write(result)
+
+exit(0)
+
+
+def usage():
+	print "Usage: \n\n" + \
+		  "python battery.py [logfile]\n\n" + \
+		  "Arguments: \n" + \
+		  "    [logfile]    The output file where the data should be appended.\n" + \
+		  "                   If no output file is specified, data is printed to stdout."
