fc75bb4fcbbe22452f292780c6cc573f7539ab00
Michael Posner <mike24hockey@gmail.com>
2016-06-18 18:18:54 -0700

    convert data while parsing, add runtime plot

--- analyze.py
+++ analyze.py
@@ -12,7 +12,7 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def usage():
     print "Usage: \n\n" + \
-          "python analysis.py [logfile] [outfile]\n\n" + \
+          "python analyze.py [logfile] [outfile]\n\n" + \
           "Arguments: \n" + \
           "    [logfile]    Input file with data written by battery.py\n" + \
           "    [outfile]    Output image file name\n"
@@ -44,24 +44,35 @@ def parseLog(logfile):
             
             #print data;
             
-            result['dates'].append(data[0])
-            result['statuses'].append(data[1].split("=")[1])
-            result['charges'].append(data[2].split("=")[1])
-            result['runtimes'].append(data[3].split("=")[1])
+            # Parse Date
+            floatdate = datetime.strptime(data[0], DATE_FORMAT)  #convert date string into datetime object
+            result['dates'].append(floatdate)
+            
+            # Parse Battery Status
+            strstatus = data[1].split("=")[1]
+            result['statuses'].append(int(strstatus))
+            
+            # Parse Charge
+            strcharge = data[2].split("=")[1]
+            result['charges'].append(int(strcharge))
+            
+            # Parse Runtime
+            strruntime = data[3].split("=")[1]
+            result['runtimes'].append(int(strruntime))
             
             dataline = f.readline()
             processline = f.readline()
         
     
     
-    print "DATES:\n"
-    print result['dates']
-    print "STATUSES:\n"
-    print result['statuses']
-    print "CHARGES:\n"
-    print result['charges']
-    print "RUNTIMES:\n"
-    print result['runtimes']
+    # print "DATES:\n"
+    # print result['dates']
+    # print "\nSTATUSES:\n"
+    # print result['statuses']
+    # print "\nCHARGES:\n"
+    # print result['charges']
+    # print "\nRUNTIMES:\n"
+    # print result['runtimes']
     
     return result
 
@@ -82,28 +93,50 @@ result = parseLog(logfile)
 
 colors = []
 for charge in result['charges']:
-    charge = int(charge)
     
     colors.append((1-charge/100.0, charge/100.0, 0.3))
 
 
-# Convert date data into usable numbers
-floatdates = [datetime.strptime(x, DATE_FORMAT) for x in result['dates']]
+# Graph data: Date vs. Charge
+f1 = pyplot.figure(1)
 
-# Graph data
-pyplot.figure(1)
-
-for i in range(len(floatdates)):
-    pyplot.plot_date(floatdates[i], result['charges'][i], 'o', color=colors[i], markersize=4, markeredgewidth=0.1)
+for i in range(len(result['dates'])):
+    pyplot.plot_date(result['dates'][i], result['charges'][i], 'o', color=colors[i], markersize=4, markeredgewidth=0.1)
 
 pyplot.ylim(0,100)
 pyplot.xlabel('Date')
-pyplot.ylabel('Charge')
+pyplot.ylabel('Charge [%]')
 pyplot.title('Laptop Battery Charge')
 pyplot.grid(True)
 pyplot.figure(1).autofmt_xdate()
 
 pyplot.savefig(outfile, dpi=500)
+
+pyplot.close(f1)
+
+
+# Graph data: Charge vs. Runtime
+f2 = pyplot.figure(2)
+
+# Remove outlier runtime data
+charges_cleaned = []
+runtimes_cleaned = []
+for i in range(len(result['runtimes'])):
+    if result['runtimes'][i] < 1000000:
+        charges_cleaned.append(result['charges'][i]) 
+        runtimes_cleaned.append(result['runtimes'][i] / 60.0)  # convert runtimes to hours 
+
+pyplot.plot(charges_cleaned, runtimes_cleaned, 'o', markersize=4, markeredgewidth=0.1)
+
+#pyplot.ylim(0,200)
+pyplot.xlabel('Battery Charge [%]')
+pyplot.ylabel('Runtime [hr]')
+pyplot.title('Laptop - Windows 7 Estimated Runtime')
+pyplot.grid(True)
+
+pyplot.savefig(outfile + "_runtime.png", dpi=500)
+
+pyplot.close(f2)
 
 
 exit(0)
