b57ef4b3206d2253c58a4825b50461035020b594
Michael Posner <mike24hockey@gmail.com>
2016-08-05 20:46:18 -0700

    Refactor and add documentation to analyze.py
    - Also tweak charge-runtime graph and update

--- analyze.py
+++ analyze.py
@@ -16,11 +16,21 @@ def usage():
           "python analyze.py [logfile] [outfile]\n\n" + \
           "Arguments: \n" + \
           "    [logfile]    Input file with data written by battery.py\n" + \
-          "    [outfile]    Output image file name\n"
+          "    [outfile]    Output image file name (without extension)\n"
 
 
 def parseLog(logfile):
-    """Reads and parses log data from file"""
+    """
+    Reads and parses log data from file,
+    
+    @param logfile: The filename (including path) of the log file to be parsed.
+    
+    @return: A dictionary of string to array mappings:
+     - 'dates' => A list of datetime objects
+     - 'statuses' => A list of integer status values
+     - 'charges' => A list of integer charge percentages
+     - 'runtimes' => A list of integer runtimes
+    """
     
     result = {}
     
@@ -29,6 +39,7 @@ def parseLog(logfile):
     result['charges'] = []
     result['runtimes'] = []
     
+    print "Parsing log"
     
     # Read input file
     with open(logfile, 'r') as f:
@@ -42,8 +53,6 @@ def parseLog(logfile):
             # [2016-02-03 20:11:23, BatteryStatus=2, EstimatedChargeRemaining=78, EstimatedRunTime=71582788, Status=OK]
             
             data = dataline.strip("[]\n").split(", ")
-            
-            #print data;
             
             # Parse Date
             floatdate = datetime.strptime(data[0], DATE_FORMAT)  #convert date string into datetime object
@@ -65,7 +74,6 @@ def parseLog(logfile):
             processline = f.readline()
         
     
-    
     # print "DATES:\n"
     # print [mktime(d.timetuple()) for d in result['dates']]
     # print "\nSTATUSES:\n"
@@ -78,6 +86,95 @@ def parseLog(logfile):
     return result
 
     
+def graph_date_v_charge(data):
+    """
+    Graphs charge data over time.
+    
+    @param data: Parsed data from log file; output of L{parseLog}
+    """
+    
+    print "Graphing Date v. Charge"
+    
+    # Build color data based on charge percentage
+    colors = []
+    for charge in data['charges']:
+        
+        colors.append((1-charge/100.0, charge/100.0, 0.3))
+
+
+    f1 = pyplot.figure(1)
+
+    for i in range(len(data['dates'])):
+        pyplot.plot_date(data['dates'][i], data['charges'][i], 'o', color=colors[i], 
+                         markersize=3, markeredgewidth=0.1)
+
+    pyplot.ylim(0,100)
+    pyplot.xlabel('Date')
+    pyplot.ylabel('Charge [%]')
+    pyplot.title('Laptop Battery Charge')
+    pyplot.grid(True)
+    pyplot.figure(1).autofmt_xdate()
+
+    pyplot.savefig(outfile + ".png", dpi=500)
+
+    pyplot.close(f1)
+    
+
+
+
+def graph_charge_v_runtime(data):
+    """
+    Graphs runtime data over charge, coloring by date of each sample.
+    Removes runtimes > 24 hours (outliers that happen when laptop is plugged into AC power)
+    
+    @param data: Parsed data from log file; output of L{parseLog}
+    """
+    
+    print "Graphing Charge v. Runtime"
+    
+    f2 = pyplot.figure(2)
+
+    # Create color data based on the sample date
+    date_start = mktime(result['dates'][0].timetuple())
+    date_end = mktime(result['dates'][-1].timetuple())
+
+    #print "start: %d, end: %d, diff: %d" % (date_start, date_end, date_end - date_start)
+    colors = []
+    for date in result['dates']:
+        d = mktime(date.timetuple())
+        colors.append(( (d - date_start) / float(date_end-date_start), 
+                        1 - (d - date_start) / float(date_end-date_start), 0.3))
+
+    
+    # Remove outlier runtime data
+    charges_cleaned = []
+    runtimes_cleaned = []
+    colors_cleaned = []
+
+    for i in range(len(result['runtimes'])):
+        if result['runtimes'][i] < 24*60:  # runtime data is considered bad if it is > 24 hours
+            charges_cleaned.append(result['charges'][i]) 
+            runtimes_cleaned.append(result['runtimes'][i] / 60.0)  # convert runtimes to hours 
+            colors_cleaned.append(colors[i])
+            
+    for i in range(len(charges_cleaned)):
+        pyplot.plot(charges_cleaned[i], runtimes_cleaned[i], 'o', color=colors_cleaned[i], 
+                   markersize=2, markeredgewidth=0.1)
+
+
+    pyplot.xlabel('Battery Charge [%]')
+    pyplot.ylabel('Remaining Runtime [hr]')
+    pyplot.title('Laptop - Windows 7 Estimated Runtime')
+    pyplot.grid(True)
+
+    pyplot.savefig(outfile + "_runtime.png", dpi=500)
+
+    pyplot.close(f2)
+
+
+
+    
+
 # ======  MAIN  ======
 
 
@@ -89,72 +186,15 @@ else:
     usage()
     exit(1)
 
+    
 # Parse log data
 result = parseLog(logfile)
 
-colors = []
-for charge in result['charges']:
-    
-    colors.append((1-charge/100.0, charge/100.0, 0.3))
-
-
 # Graph data: Date vs. Charge
-f1 = pyplot.figure(1)
-
-for i in range(len(result['dates'])):
-    pyplot.plot_date(result['dates'][i], result['charges'][i], 'o', color=colors[i], markersize=3, markeredgewidth=0.1)
-
-pyplot.ylim(0,100)
-pyplot.xlabel('Date')
-pyplot.ylabel('Charge [%]')
-pyplot.title('Laptop Battery Charge')
-pyplot.grid(True)
-pyplot.figure(1).autofmt_xdate()
-
-pyplot.savefig(outfile, dpi=500)
-
-pyplot.close(f1)
-
+graph_date_v_charge(result)
 
 # Graph data: Charge vs. Runtime
-f2 = pyplot.figure(2)
-
-# create color data based on the sample date
-date_start = mktime(result['dates'][0].timetuple())
-date_end = mktime(result['dates'][-1].timetuple())
-
-
-#print "start: %d, end: %d, diff: %d" % (date_start, date_end, date_end - date_start)
-colors = []
-for date in result['dates']:
-    d = mktime(date.timetuple())
-    colors.append(( (d - date_start) / float(date_end-date_start), 
-                    1 - (d - date_start) / float(date_end-date_start), 0.3))
-
-# Remove outlier runtime data
-charges_cleaned = []
-runtimes_cleaned = []
-colors_cleaned = []
-
-for i in range(len(result['runtimes'])):
-    if result['runtimes'][i] < 24*60:
-        charges_cleaned.append(result['charges'][i]) 
-        runtimes_cleaned.append(result['runtimes'][i] / 60.0)  # convert runtimes to hours 
-        colors_cleaned.append(colors[i])
-        
-for i in range(len(charges_cleaned)):
-    pyplot.plot(charges_cleaned[i], runtimes_cleaned[i], 'o', color=colors_cleaned[i], 
-               markersize=3, markeredgewidth=0.1)
-
-
-pyplot.xlabel('Battery Charge [%]')
-pyplot.ylabel('Runtime [hr]')
-pyplot.title('Laptop - Windows 7 Estimated Runtime')
-pyplot.grid(True)
-
-pyplot.savefig(outfile + "_runtime.png", dpi=500)
-
-pyplot.close(f2)
+graph_charge_v_runtime(result)
 
 
 exit(0)
